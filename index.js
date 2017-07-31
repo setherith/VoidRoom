054a1423df705a929fcd9aba1b7d1d83ef5265c9
Shane <setherith@hotmail.co.uk>
2017-07-31 19:40:57 +0100

    Adding styles and tidy up

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ app.engine('hbs', cons.handlebars);
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/public', express.static(__dirname + '/public'));
+
 app.use('/', function(req, res) {
     bars.registerPartial('content', fs.readFileSync(__dirname + '/views/index.hbs', 'utf8'));
     res.render('layout', {
-        title : 'This is a test'
+        title : 'Home'
     });
 });
 
