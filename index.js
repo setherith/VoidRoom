ffc12903b37ab4c7726260f0b293c940a9cc54cd
Shane Pudner <setherith@gmail.com>
2017-08-04 11:58:00 +0100

    Switched dependancies to get layout working

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-var fs = require('fs');
-var cons = require('consolidate');
-var bars = require('handlebars');
+var hbs = require('hbs');
 
 var express = require('express');
 var app = express();
 
-app.engine('hbs', cons.handlebars);
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
@@ -13,13 +10,14 @@ app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use('/', function(req, res) {
-    bars.registerPartial('content', fs.readFileSync(__dirname + '/views/index.hbs', 'utf8'));
-    res.render('layout', {
-        title : 'Home'
-    });
+app.get('/', function(req, res) {
+    res.render('index', {title: 'Home'});
+});
+
+app.get('/add', function(req, res) {
+    res.render('add', {title: 'Add Room'});
 });
 
 app.listen(3000, function() {
-    console.log('Port: ' + 3000);
+    console.log('Started...');
 });
